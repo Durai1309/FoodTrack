3dc45df42e82387248e8690571725980542352dd
Durai1309 <durairaj14092000@gmail.com>
2023-05-10 00:24:06 +0530

    Initial Setup

---
+++ FoodTrack.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,36 @@
+﻿using FoodTrack.Services.ProductAPI.Models.Dto;
+using FoodTrack.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTrack.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _responseDto;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._responseDto = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _responseDto.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.Success = false;
+            }
+            return _responseDto;
+        }
+
+
+    }
+}
