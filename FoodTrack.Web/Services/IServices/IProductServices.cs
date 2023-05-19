7b67ead84db747afc8ed2f08b68495ce356e56a7
Durai1309 <durairaj14092000@gmail.com>
2023-05-19 23:42:21 +0530

    Initial Setup

--- FoodTrack.Web/Services/IServices/IProductServices.cs
+++ FoodTrack.Web/Services/IServices/IProductServices.cs
@@ -4,6 +4,11 @@ namespace FoodTrack.Web.Services.IServices
 {
     public interface IProductServices : IBaseSevices
     {
+        /// <summary>
+        /// GetAllProductAsync
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         Task<T> GetAllProductAsync<T>();
         Task<T> GetProductByIdAsync<T>(int id);
         Task<T> CreateProductTAsync<T>(ProductDto productDto);
