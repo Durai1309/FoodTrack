4e98d1a8abb918aa10477495362138404637fe8e
Durai1309 <durairaj14092000@gmail.com>
2023-06-04 21:01:55 +0530

    Initial Setup

--- FoodTrack.Web/Controllers/ProductController.cs
+++ FoodTrack.Web/Controllers/ProductController.cs
@@ -11,98 +11,94 @@ using System.Reflection;
 
 namespace FoodTrack.Web.Controllers
 {
-    public class ProductController : Controller
-    {
-        private readonly IProductServices _services;
-		
+	public class ProductController : Controller
+	{
+		private readonly IProductServices _services;
+
 		public ProductController(IProductServices productServices)
-        {
-            _services = productServices;
-        }
-        public async Task<IActionResult> ProductIndex()
-        {
-            List<ProductDto> list = new();
-            var response = await _services.GetAllProductAsync<ResponseDto>();
-            if (response != null && response.Success)
-            {
-                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
-            }
-            return View(list);
-        }
-        public async Task<IActionResult> ProductCreate()
-        {
-            return View();
-        }
-		
+		{
+			_services = productServices;
+		}
+		public async Task<IActionResult> ProductIndex()
+		{
+			List<ProductDto> list = new();
+			var response = await _services.GetAllProductAsync<ResponseDto>();
+			if (response != null && response.Success)
+			{
+				list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+			}
+			return View(list);
+		}
+		public async Task<IActionResult> ProductCreate()
+		{
+			return View();
+		}
+
 		[HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ProductCreate(ProductDto model)
-        {
-            if (ModelState.IsValid)
-            {
-                var response = await _services.CreateProductTAsync<ResponseDto>(model);
-                if (response != null && response.Success)
-                {
-                    return RedirectToAction(nameof(ProductIndex));
-                }
-            }
-            return View(model);
-        }
-	
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> ProductCreate(ProductDto model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _services.CreateProductTAsync<ResponseDto>(model);
+				if (response != null && response.Success)
+				{
+					return RedirectToAction(nameof(ProductIndex));
+				}
+			}
+			return View(model);
+		}
+
 		public async Task<IActionResult> ProductEdit(int ProductID)
-        {
-            var response = await _services.GetProductByIdAsync<ResponseDto>(ProductID);
-            if (response != null && response.Success)
-            {
-                ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
-                return View(model);
-            }
-            return NotFound();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ProductEdit(ProductDto model)
-        {
-            if (ModelState.IsValid)
-            {
-                var response = await _services.UpdateProductTAsync<ResponseDto>(model);
-                if (response != null && response.Success)
-                {
-                    return RedirectToAction(nameof(ProductIndex));
-                }
-            }
-            return View(model);
-        }
-		/// <summary>
-		/// ProductDelete
-		/// </summary>
-		/// <param name="productId"></param>
-		/// <returns></returns>
+		{
+			var response = await _services.GetProductByIdAsync<ResponseDto>(ProductID);
+			if (response != null && response.Success)
+			{
+				ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+				return View(model);
+			}
+			return NotFound();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> ProductEdit(ProductDto model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _services.UpdateProductTAsync<ResponseDto>(model);
+				if (response != null && response.Success)
+				{
+					return RedirectToAction(nameof(ProductIndex));
+				}
+			}
+			return View(model);
+		}
+
 		public async Task<IActionResult> ProductDelete(int productId)
-        {
-            var response = await _services.GetProductByIdAsync<ResponseDto>(productId);
-            if (response != null && response.Success)
-            {
-                ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
-                return View(model);
-            }
-            return NotFound();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ProductDelete(ProductDto model)
-        {
-            if (ModelState.IsValid)
-            {
-                var response = await _services.DeleteProductTAsync<ResponseDto>(model.ProductID);
-                if (response.Success)
-                {
-                    return RedirectToAction(nameof(ProductIndex));
-                }
-            }
-            return View(model);
-        }
-    }
+		{
+			var response = await _services.GetProductByIdAsync<ResponseDto>(productId);
+			if (response != null && response.Success)
+			{
+				ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+				return View(model);
+			}
+			return NotFound();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> ProductDelete(ProductDto model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _services.DeleteProductTAsync<ResponseDto>(model.ProductID);
+				if (response.Success)
+				{
+					return RedirectToAction(nameof(ProductIndex));
+				}
+			}
+			return View(model);
+		}
+	}
 
 }
 
